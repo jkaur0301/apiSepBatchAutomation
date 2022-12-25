22b6257078a624b5faac4976506b63c5d9760b07
Asus <Asus@192.168.2.17>
2022-12-25 00:44:21 -0500

    Added code for surefire plugin for execution from pom.xml

--- API_Sep_FW/src/test/java/org/testing/testCases/TC1_PostRequest.java
+++ API_Sep_FW/src/test/java/org/testing/testCases/TC1_PostRequest.java
@@ -2,6 +2,7 @@ package org.testing.testCases;
 
 import java.io.IOException;
 import java.util.Properties;
+import java.util.Random;
 import java.util.Scanner;
 
 import org.testing.utilities.HandleJsonFile;
@@ -22,11 +23,15 @@ public class TC1_PostRequest
 	{
 		Properties probject = HandleProperties.loadPropertiesFile("../API_Sep_FW/URI.properties");
 		String inputBodydata= HandleJsonFile.readJson("../API_Sep_FW/src/test/java/org/testing/resources/Request.json");
-		System.out.println("Please enter id value: ");
-		Scanner s=new Scanner(System.in);
-		String idvalue=s.next() ;
+	//	System.out.println("Please enter id value: ");
+	//	Scanner s=new Scanner(System.in);
+	//	String idvalue=s.next() ;
 		
-		String updateBodyData=JsonReplacement.jsonDataVariableValue(inputBodydata, "id", idvalue);
+	//	String updateBodyData=JsonReplacement.jsonDataVariableValue(inputBodydata, "id", idvalue);
+		
+		Random r=new Random();
+		Integer idvalue=r.nextInt();
+		String updateBodyData=JsonReplacement.jsonDataVariableValue(inputBodydata, "id", idvalue.toString());
 		HTTPMethods http=new HTTPMethods(probject);
 		Response resObject=http.postrequest("QA_URI", updateBodyData);
 		residvalue=JsonParsingUsingJsonPath.jsonParse(resObject,"id");
